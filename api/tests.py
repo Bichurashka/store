ac7ebbd27be409091e7c8d2ee479c97e9d5f9d4f
Bichurashka <61475413+Bichurashka@users.noreply.github.com>
2025-06-25 18:09:31 +0300

    AM-4: linters, formatters (#4)
    Co-authored-by: Bichurashka <bichurash@gmail.com>

--- api/tests.py
+++ api/tests.py
@@ -1,23 +1,23 @@
+from unittest.mock import MagicMock, patch
+
 from django.db import OperationalError
-from django.test import TestCase
-from unittest.mock import patch
-from rest_framework import status
-from django.test import Client
+from django.test import Client, TestCase
 from django.urls import reverse
+from rest_framework import status
 
 
 class HealthCheckTests(TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.client = Client()
 
-    def test_health_check_success(self):
-        response = self.client.get(reverse('health_check'))
+    def test_health_check_success(self) -> None:
+        response = self.client.get(reverse("health_check"))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    @patch('api.views.connections')
-    def test_health_check_db_error(self, mock_connections):
+    @patch("api.views.connections")
+    def test_health_check_db_error(self, mock_connections: MagicMock) -> None:
         mock_conn = mock_connections.__getitem__.return_value
         mock_conn.cursor.side_effect = OperationalError("DB connection error")
 
-        response = self.client.get(reverse('health_check'))
+        response = self.client.get(reverse("health_check"))
         self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
--- api/views.py
+++ api/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from django.db import connections
 from django.db.utils import OperationalError
-
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 # Create your views here.
 
-@api_view(['GET'])
-def health_check(request):
-    conn = connections['default']
+
+@api_view(["GET"])
+def health_check(request: Request) -> Response:
+    conn = connections["default"]
     try:
-        c = conn.cursor()
+        conn.cursor()
     except OperationalError:
         return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     return Response(status=status.HTTP_200_OK)
